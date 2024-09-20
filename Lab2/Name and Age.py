60d8158b594aac1409d8b3c852cc13153b2785b3
Splodge0007 <44043043+Splodge0007@users.noreply.github.com>
2024-09-20 12:00:15 +0100

    Add files via upload

---
+++ Lab2/Name and Age.py
@@ -0,0 +1,9 @@
+name = input("What is your name?")
+age = int(input("How old are you?"))
+
+string1 = "Hello "
+string2 = ". You don't look a day over "
+
+newage = str(age - 5)
+
+print(string1 + name + string2 + newage)
---
+++ Lab2/Spam.py
@@ -0,0 +1,5 @@
+num1 = int(input("How many tins do you want?"))
+#per tin cost = 3
+#p+p cost = 7
+answer = (num1*3)+7
+print("The total is ", answer)
